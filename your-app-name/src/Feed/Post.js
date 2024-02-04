9f1dee2a8906bd128ad581e2fb1e7f0e06f4282f
Fatma434 <fatmamuuhmed99@gmail.com>
2024-02-04 08:23:06 +0200

    update

--- your-app-name/src/Feed/Post.js
+++ your-app-name/src/Feed/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from "react"
 import { Avatar } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
@@ -7,34 +7,34 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import PublishIcon from '@mui/icons-material/Publish';
 import './Post.css';
 
-function Post({
+const Post = forwardRef(({
     displayName,
     userName,
     verified,
     text,
     image,
-    timestamp,
-    te
-}) {
+    avatar
+    
+},ref)=> {
   return (
-    <div className='post'>
+    <div className='post' ref={ref}>
         <div className='post__avatar'>
-          <Avatar alt="Remy Sharp" src="https://i.cbc.ca/1.7046282.1701447456!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/humane-society.jpg" />
+          <Avatar src={avatar}/>
         </div>
         <div className='post__body'>
             <div className='post__header'>
                 <h3>
-                  Suga-bts {""}
+                  {displayName} {""}
                   <span className='post__headerSpecial'
-                  ><CheckCircleIcon className="post__badge"  /> @fgf</span>
+                  >{verified &&<CheckCircleIcon className="post__badge"  />} @{userName}</span>
                 </h3>
             </div>
             <div className='post__headerDescription'>
-              <p> hiiiiiiiiiiii , everybody </p>
+              <p> {text}</p>
             </div>
         
         <img  
-        src="https://img.wattpad.com/156181e751adcfdd476ba2bd113e390be52a66e2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f5f2d7762747a5343736771374a513d3d2d323835382e313562333739663431363835353961313339363036313733303832392e676966" alt=""/>
+        src={image} alt=""/>
   
   <div className='post__footer'>
   <ChatBubbleOutlineIcon  fontSize="small"  />
@@ -46,5 +46,5 @@ function Post({
     </div>
   );
 }
-
+);
 export default Post;
